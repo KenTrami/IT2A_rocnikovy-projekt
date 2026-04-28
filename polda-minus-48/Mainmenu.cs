da0c0b206c774041c1f13d9ec0abdef27407b32b
KrystofAdamec <adameckr@gmail.com>
2026-04-28 19:49:22 +0200

    Doplnění provizorních funkcí pro tlačítka
    - Každé tlačítko teď píše něco do konzole (konzole bude později nahrazena grafickým textem přímo ve hře (možná i dabingem)
    - přidání interakcí (sebrání předmětu)

--- polda-minus-48/Mainmenu.cs
+++ polda-minus-48/Mainmenu.cs
@@ -3,6 +3,7 @@ using System;
 
 public partial class Mainmenu : Control
 {
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -25,8 +26,58 @@ public partial class Mainmenu : Control
 	}
 	private void _on_zamknute_dvere()
 	{
+		
 		GD.Print("Sakra, je zamčeno!");
 		
 	}
-	
+	bool zhasnuto=true;
+	private void _on_zhasnout_světlo_pressed()
+	{
+		
+		if (!zhasnuto == false)
+		{
+			GetNode<Sprite2D>("../Room1Rosviceno").Hide();	//.. - zkratka pro node
+			GD.Print("Konečne ty zářivky nesvítí jak stroboskop.");
+			zhasnuto=false;
+		}
+		else
+		{
+			GD.Print("Nevím proč bych to zapínal.");
+		}
+
+	}
+	private void _on_podívat_se_za_tabuli_pressed()
+	{
+		GD.Print("Jé hele míček! Škoda že na něho nedošáhnu.");
+	}
+	bool PravitkoNaStole=true;
+	private void _on_sebrat_pravítko_pressed()
+	{
+		if(!PravitkoNaStole==false)
+		GD.Print("Pravítko. Beru ho, může se hodit.");
+		GetNode<Sprite2D>("../Pravítko").Hide(); //.. - zkratka pro node
+	}	
+	private void _on_podivně_svítící_zářivka_pressed()
+	{
+		if (!zhasnuto == false)
+		{
+			GD.Print("Ta zářivka je ňáká divná. Jak kdyby v ní něco bylo.");	
+		}
+		else
+		{
+			GD.Print("Hele Klíče!. Musím se pro ně nějak dostat.");
+		}
+		
+	}
+	private void _on_podívat_se_z_okna_pressed()
+	{
+		if (!zhasnuto == false)
+		{
+			GD.Print("Kdyby to světlo tolik nesvítilo, tak možná něco viděl.");
+		}
+		else
+		{
+			GD.Print("Hm... Tak tudy to nepude. Za prvé tam jsou venkovní žaluzie a za druhé tam je díra jak doprd.. teda do propasti hehe.");
+		}
+	}
 }
