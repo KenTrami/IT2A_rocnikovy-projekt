b7726410cbe900316b4f9974b2d5868eea03941b
KrystofAdamec <adameckr@gmail.com>
2026-05-07 14:28:00 +0200

    Práce 7.5.

---
+++ polda-minus-48/Scripts/PodivatSeZaTabuli.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+public partial class PodivatSeZaTabuli : Button
+{
+	private Label label;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		label=GetNode<Label>("/root/Node/Controls/CanvasLayer/Panel/Label");
+		
+		Pressed += _on_podivat_se_za_tabuli_pressed;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+	private void _on_podivat_se_za_tabuli_pressed()
+	{
+		//GD.Print("");
+		//GetNode<Label>("CanvasLayer/Label").Text = "Ahoj světe!";
+		//GetNode<Label>("/root/Node/Controls/CanvasLayer/Panel/Label").Text = "Ahoj" ;
+		//label.Text = "Ahoj světe!";
+		label.Text="Jé hele míček! Škoda že na něho nedošáhnu.";
+
+	}
+}
---
+++ polda-minus-48/Scripts/TlackaSeSvetlem.cs
@@ -0,0 +1,64 @@
+using Godot;
+using System;
+
+public partial class TlackaSeSvetlem : Button
+{
+	private Label label;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		label=GetNode<Label>("/root/Node/Controls/CanvasLayer/Panel/Label");
+			
+		
+		Pressed +=  _on_zhasnout_svetlo_pressed;
+		Pressed += _on_podivne_svitici_zarivka_pressed;
+		Pressed += _on_podivat_se_z_okna_pressed;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+	bool zhasnuto=true;
+	private void _on_zhasnout_svetlo_pressed()
+	{
+		
+		if (!zhasnuto == false)
+		{
+			GetNode<Sprite2D>("../Room1_Rosviceno").Hide();	//.. - zkratka pro node
+			label.Text="Konečne ty zářivky nesvítí jak stroboskop.";
+			zhasnuto=false;
+			
+		}
+		else
+		{
+			label.Text="Nevím proč bych to zapínal.";
+		}
+
+	}
+	private void _on_podivne_svitici_zarivka_pressed()
+	{
+		if (!zhasnuto == false)
+		{
+			label.Text="Ta zářivka je ňáká divná. Jak kdyby v ní něco bylo.";	
+		}
+		else
+		{
+			label.Text="Hele Klíče!. Musím se pro ně nějak dostat.";
+		}
+		
+	}
+	
+	private void _on_podivat_se_z_okna_pressed()
+		{
+			if (!zhasnuto == false)
+			{
+				
+				label.Text="Kdyby to světlo tolik nesvítilo, tak možná něco viděl.";
+			}
+			else
+			{
+				label.Text="Hm... Tak tudy to nepude. Za prvé tam jsou venkovní žaluzie a za druhé tam je díra jak dopr.. teda do propasti hehe.";
+			}
+		}
+}
